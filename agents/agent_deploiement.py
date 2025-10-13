b30ec9166693b7a66b477e0f5de030e2e1e5710d
Hot-Prod <L.garnier@hotmail.com>
2025-10-13 16:52:48 +0200

    Create agent_deploiement.py

---
+++ agents/agent_deploiement.py
@@ -0,0 +1,5 @@
+import os
+os.system("git add index.html")
+os.system('git commit -m "MAJ auto site HotProd"')
+os.system("git push origin main")
+print("🚀 Site HotProd mis à jour sur GitHub Pages.")
